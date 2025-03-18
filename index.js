37a390056d428405a7ca1f8e9dca36b96a43e1e3
Kingbanda <bandadesignske@gmail.com>
2025-03-18 21:58:56 +0300

    Scope lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "Mercy";
+
+
+function getCustomerName (customerName){
+ return customerName;
+}
+
+console.log(getCustomerName ());
+
+function upperCaseCustomerName(customerName) {
+  customerName = customerName.toUpperCase();
+
+  return customerName();
+}
+console.logupperCaseCustomerName(customerName)
+
+
+function setBestCustomer(bestCustomer){
+bestCustomer = 'not bob';
+return "bestCustomer"
+}
+console.log(setBestCustomer(bestCustomer))
+
+
+function overwriteBestCustomer(bestCustomer){
+bestCustomer = 'maybe bob';
+
+return "bestCustomer"
+}
+console.log(overwriteBestCustomer(bestCustomer));
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Osman";
+
+
+}
+
+
+
+
